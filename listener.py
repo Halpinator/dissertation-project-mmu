354873971466a7e2f917e80858a7bde1b69a55dd
David Halpin-Roberts <davidhalpinroberts@gmail.com>
2023-04-17 16:30:42 +0100

    Listener now records 3 seconds of audio after wake word

--- listener.py
+++ listener.py
@@ -1,6 +1,25 @@
 #Listens for wake word and records 5 seconds of audio after it is heard
 import pvporcupine
 from pvrecorder import PvRecorder
+import pyaudio
+import wave
+
+
+
+# the file name output you want to record into
+filename = "recorded.wav"
+# set the chunk size of 1024 samples
+chunk = 1024
+# sample format
+FORMAT = pyaudio.paInt16
+# mono, change to 2 if you want stereo
+channels = 1
+# 44100 samples per second
+sample_rate = 44100
+record_seconds = 5
+# initialize PyAudio object
+p = pyaudio.PyAudio()
+
 
 keywords = ['alexa']
 
@@ -10,7 +29,6 @@ porcupine = pvporcupine.create(
 )
 
 recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
-
 
 try:
     recorder.start()
@@ -23,9 +41,49 @@ try:
         if keyword_index >= 0:
             print(f"Detected {keywords[keyword_index]}")
 
+            # Stop the recordeer
+            recorder.stop()
+
+            # open stream object as input & output
+            stream = p.open(format=FORMAT,
+                            channels=channels,
+                            rate=sample_rate,
+                            input=True,
+                            output=True,
+                            frames_per_buffer=chunk)
+            frames = []
+            print("Recording...")
+            for i in range(int(sample_rate / chunk * record_seconds)):
+                data = stream.read(chunk)
+                # if you want to hear your voice while recording
+                # stream.write(data)
+                frames.append(data)
+            print("Finished recording.")
+            # stop and close stream
+            stream.stop_stream()
+            stream.close()
+            # save audio file
+            # open the file in 'write bytes' mode
+            wf = wave.open(filename, "wb")
+            # set the channels
+            wf.setnchannels(channels)
+            # set the sample format
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            # set the sample rate
+            wf.setframerate(sample_rate)
+            # write the frames as bytes
+            wf.writeframes(b"".join(frames))
+            # close the file
+            wf.close()
+
+            # Start the recorder again
+            recorder.start()
+
 
 except KeyboardInterrupt:
     recorder.stop()
+    # terminate pyaudio object
+    p.terminate()
 finally:
     porcupine.delete()
     recorder.delete()
